04a8aa6e5bff4a59fd79af74aa7f53188e0557c6
suroh <mx@suroh.tk>
2023-05-16 12:06:52 +0200

    added skeleton of summary page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import FormView from '../views/FormView.vue';
 import FormInfo from '../views/FormInfo.vue';
 import FormPlan from '../views/FormPlan.vue';
 import FormAdd from '../views/FormAdd.vue';
+import FormSummary from '../views/FormSummary.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,8 +22,12 @@ const router = createRouter({
           component: FormPlan,
         },
         {
-          path: 'add',
+          path: 'addons',
           component: FormAdd,
+        },
+        {
+          path: 'summary',
+          component: FormSummary,
         },
       ],
     },
---
+++ src/views/FormSummary.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import OptionIcon from '../components/OptionIcon.vue'
+</script>
+
+<template>
+  <div class="flow">
+    <h2>Finishing Up</h2>
+    <p>Double-check everything looks OK before confirming.</p>
+  </div>
+
+  <section>
+    <div class="plan">
+
+    </div>
+    <div class="addons">
+
+    </div>
+  </section>
+
+  <div class="total">
+
+  </div>
+</template>
+
+<style scoped>
+</style>
